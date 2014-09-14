1cafcf2cbd0230ab6cc4bf3f7e20764a2ff5ca39
Lisa Marie Maginnis <lisam@fsf.org>
2014-09-14 15:11:02 -0400

    Added fsf extras repo

--- scripts/FSF/00_setup_repository.sh
+++ scripts/FSF/00_setup_repository.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-fcopy -v /etc/apt/sources.list.d/fsf-extras
+fcopy -v /etc/apt/sources.list.d/fsf-extras.list
 $ROOTCMD wget mirror.fsf.org/fsfsys-trisquel/conf/lisam.gpg.key -O- | $ROOTCMD apt-key add -
