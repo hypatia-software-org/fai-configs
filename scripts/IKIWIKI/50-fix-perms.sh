37e97edb0bd38ffc029f057227b020d45c326c2a
Lisa Marie Maginnis <lisam@fsf.org>
2015-01-26 16:49:30 -0500

    more perms for wiki

--- scripts/IKIWIKI/50-fix-perms.sh
+++ scripts/IKIWIKI/50-fix-perms.sh
@@ -7,5 +7,7 @@ $ROOTCMD chown -R ikiwiki.www-data /var/www/html/wiki
 $ROOTCMD find /var/www/html/wiki -type d -print0 | xargs -0 chmod 750
 $ROOTCMD find /var/www/html/wiki -type f ! -name '*cgi' -print0 | xargs -0 chmod 640
 $ROOTCMD chmod 6750 /var/www/html/wiki/ikiwiki.cgi
+$ROOTCMD chown -R ikiwiki.www-data /srv/ikiwiki
 $ROOTCMD find /srv/ikiwiki -type d -print0 | xargs -0 chmod 750
 $ROOTCMD find /srv/ikiwiki -type f -print0 | xargs -0 chmod 640
+
