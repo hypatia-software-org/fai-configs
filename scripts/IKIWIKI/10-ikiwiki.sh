ee1b8211e205f69b333ae4e4ab1cf21b6cb90491
Lisa Marie Maginnis <lisam@fsf.org>
2015-01-27 11:39:31 -0500

    added keygen

--- scripts/IKIWIKI/10-ikiwiki.sh
+++ scripts/IKIWIKI/10-ikiwiki.sh
@@ -5,5 +5,5 @@ if ! $ROOTCMD getent passwd ikiwki ; then
     $ROOTCMD adduser --system --ingroup www-data --disabled-password --gecos "ikiwiki user" ikiwiki
 
 if ! $ROOTCMD test -e /home/ikiwiki/.ssh/id_rsa; then
-    $ROOTCMD echo | $ROOTCMD ssh-keygen -N ''
+    $ROOTCMD echo | $ROOTCMD sudo -u ikiwiki ssh-keygen -N ''
 fi
