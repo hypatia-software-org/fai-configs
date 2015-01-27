f0c44cb51e0f4a82d2dadf584d13ba2d04ba9133
Lisa Marie Maginnis <lisam@fsf.org>
2015-01-27 11:40:24 -0500

    fixed logic

--- scripts/IKIWIKI/10-ikiwiki.sh
+++ scripts/IKIWIKI/10-ikiwiki.sh
@@ -3,6 +3,7 @@
 # add a ikiwiki user account
 if ! $ROOTCMD getent passwd ikiwki ; then
     $ROOTCMD adduser --system --ingroup www-data --disabled-password --gecos "ikiwiki user" ikiwiki
+fi
 
 if ! $ROOTCMD test -e /home/ikiwiki/.ssh/id_rsa; then
     $ROOTCMD echo | $ROOTCMD sudo -u ikiwiki ssh-keygen -N ''
