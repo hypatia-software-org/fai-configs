025ba1f73692eebe84efe0c24ec63d8aea2e03c7
Ruben Rodriguez <ruben@gnu.org>
2015-09-02 16:30:42 -0500

    Run apt in noninteractive mode in custom scripts

--- scripts/FSF/10_get_extras.sh
+++ scripts/FSF/10_get_extras.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-$ROOTCMD apt-get update
-$ROOTCMD apt-get install spd-perl
+export DEBIAN_FRONTEND=noninteractive
+
+$ROOTCMD apt-get -y --force-yes update
+$ROOTCMD apt-get -y --force-yes install spd-perl
