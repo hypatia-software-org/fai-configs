a72f8c44255ea44c82e292631020fb99bcf624c8
Lisa Marie Maginnis <lisam@fsf.org>
2016-02-28 02:43:27 -0500

    init scripts for gmg

--- scripts/GMG/15_install_gmg.sh
+++ scripts/GMG/15_install_gmg.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 fcopy -Bv /etc/init.d/install-mediagoblin
+fcopy -Bv /etc/init.d/mediagoblin-paster
+fcopy -Bv /etc/init.d/mediagoblin-celery-worker
+
 $ROOTCMD update-rc.d install-mediagoblin defaults
 sed 's^GMG_PATH_TOKEN^'$GMG_PATH'^g' -i $target/etc/init.d/install-mediagoblin
+sed 's^GMG_PATH_TOKEN^'$GMG_PATH'^g' -i $target/etc/init.d/mediagoblin-celery-worker
+sed 's^GMG_PATH_TOKEN^'$GMG_PATH'^g' -i $target/etc/init.d/mediagoblin-paster
