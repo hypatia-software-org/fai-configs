4347a31b0eccabca4843045b4291a51bc452c784
Lisa Marie Maginnis <lisam@fsf.org>
2016-02-28 01:19:27 -0500

    made gmg 05 rerunable

--- scripts/GMG/05_create_local_user.sh
+++ scripts/GMG/05_create_local_user.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Create mediagoblin user & group
-$ROOTCMD useradd -c "GNU MediaGoblin system account" -d /var/lib/mediagoblin -m -r -g www-data mediagoblin
-$ROOTCMD groupadd mediagoblin && sudo usermod --append -G mediagoblin mediagoblin
+if ! $ROOTCMD getent passwd mediagoblin ; then
+    $ROOTCMD useradd -c "GNU MediaGoblin system account" -d /var/lib/mediagoblin -m -r -g www-data mediagoblin
+    $ROOTCMD groupadd mediagoblin && sudo usermod --append -G mediagoblin mediagoblin
+fi
