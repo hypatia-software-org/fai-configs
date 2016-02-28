a7362b30200bff67da8cf2af173c303eb0747841
Lisa Marie Maginnis <lisam@fsf.org>
2016-02-28 02:00:23 -0500

    fixed sed command again, teehee

--- scripts/GMG/15_install_gmg.sh
+++ scripts/GMG/15_install_gmg.sh
@@ -2,4 +2,4 @@
 
 fcopy -Bv /etc/init.d/install-mediagoblin
 $ROOTCMD update-rc.d install-mediagoblin defaults
-sed 's_GMG_PATH_TOKEN_'$GMG_PATH'_g' -i $target/etc/init.d/install-mediagoblin
+sed 's^GMG_PATH_TOKEN^'$GMG_PATH'^g' -i $target/etc/init.d/install-mediagoblin
