ef9a35773559a0d7102cefc7d61c4128921e4276
Lisa Marie Maginnis <lisam@fsf.org>
2016-02-28 23:04:00 -0500

    automated letsencrypt

--- scripts/LETSENCRYPT/00_fetch.sh
+++ scripts/LETSENCRYPT/00_fetch.sh
@@ -4,3 +4,5 @@ if [ ! -d $LETSENCRYPT_PATH/letsencrypt ]; then
     cd $target/$LETSENCRYPT_PATH
     git clone https://github.com/letsencrypt/letsencrypt
 fi
+fcopy -Bv /etc/init.d/request-letsencrypt
+$ROOTCMD update-rc.d request-letsencrypt defaults
