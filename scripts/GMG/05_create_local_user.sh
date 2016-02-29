b4d46556d996c6544cec7efd70571503b1caea4b
Lisa Marie Maginnis <lisam@fsf.org>
2016-02-28 19:16:46 -0500

    fixed a missing ROOTCMD

--- scripts/GMG/05_create_local_user.sh
+++ scripts/GMG/05_create_local_user.sh
@@ -3,5 +3,5 @@
 # Create mediagoblin user & group
 if ! $ROOTCMD getent passwd mediagoblin ; then
     $ROOTCMD useradd -c "GNU MediaGoblin system account" -d /var/lib/mediagoblin -m -r -g www-data mediagoblin
-    $ROOTCMD groupadd mediagoblin && sudo usermod --append -G mediagoblin mediagoblin
+    $ROOTCMD groupadd mediagoblin && $ROOTCMD usermod --append -G mediagoblin mediagoblin
 fi
