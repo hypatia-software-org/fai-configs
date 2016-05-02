344f9941b63e1c44cef21201fc10455a0392393c
Tiffany <syn@synerfiles.info>
2016-05-02 00:26:14 +0000

    proxy-init

---
+++ scripts/PROXY/00-enable_apache_modules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apachedir=/etc/apache2
+emod=$apachedir/mods-enabled
+amod=$apachedir/mods-available
+
+mods=("proxy.conf" "proxy.load" "proxy_connect.load" "proxy_http.load" "proxy_html.load" "ssl.conf" "ssl.load")
+
+for mod in "${mods[@]}"
+do
+    $ROOTCMD ln -s $amod/$mod $emod/$mod
+done
