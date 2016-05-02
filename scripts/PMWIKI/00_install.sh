50f84fe5644edd03faab5c6b3f6e4b73c56c2415
Lisa Marie <elisa@ftee.org>
2016-05-02 00:30:10 +0000

    fixed install bugs

--- scripts/PMWIKI/00_install.sh
+++ scripts/PMWIKI/00_install.sh
@@ -2,12 +2,12 @@
 
 if [[ ! -d $target/var/www/$SERVERNAME ]]; then
     $ROOTCMD rm -rf /var/www/html
-    $ROOTCMD wget -O- http://www.pmwiki.org/pub/pmwiki/pmwiki-2.2.86.tgz | tar xzf -
+    wget -O- http://www.pmwiki.org/pub/pmwiki/pmwiki-2.2.86.tgz | tar xzf - -C$target/var/www/
     $ROOTCMD mv /var/ww/pmwiki-2.2.86 /var/www/$SERVERNAME
 
     cat <<EOF >$target/etc/apache2/sites-available/$SERVERNAME.conf
 <VirtualHost _default_:80>
-   ServerName $SERVERNAME
+   ServerName  $SERVERNAME
    ServerAlias $HOSTNAME
    ServerAdmin sysadmin@ftee.org
 
@@ -18,11 +18,12 @@ if [[ ! -d $target/var/www/$SERVERNAME ]]; then
       Require all granted
    </Directory>
 
-   ErrorLog ${APACHE_LOG_DIR}/http-$HOSTNAME.error.log
-   CustomLog ${APACHE_LOG_DIR}/http-$HOSTNAME.access.log combined
+   ErrorLog \${APACHE_LOG_DIR}/http-$HOSTNAME.error.log
+   CustomLog \${APACHE_LOG_DIR}/http-$HOSTNAME.access.log combined
 
 </VirtualHost>
 
 EOF
+    $ROOTCMD a2dissite 00-default
     $ROOTCMD a2ensite $SERVERNAME
 fi
