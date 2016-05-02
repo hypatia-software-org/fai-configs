d7abaf7f6724914fad5de746e5a3058def7b7475
Tiffany <syn@synerfiles.info>
2016-05-02 00:31:56 +0000

    more modules

--- scripts/PROXY/00-enable_apache_modules.sh
+++ scripts/PROXY/00-enable_apache_modules.sh
@@ -4,7 +4,7 @@ apachedir=/etc/apache2
 emod=$apachedir/mods-enabled
 amod=$apachedir/mods-available
 
-mods=("proxy.conf" "proxy.load" "proxy_connect.load" "proxy_http.load" "proxy_html.load" "ssl.conf" "ssl.load")
+mods=("proxy.conf" "proxy.load" "proxy_connect.load" "proxy_http.load" "proxy_html.load" "ssl.conf" "ssl.load" "xml2enc.load" "socache_memcache.load")
 
 for mod in "${mods[@]}"
 do
