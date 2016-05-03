2709c5eddc163cbe1865427b2dfd6a1e73c060d9
Tiffany <syn@synerfiles.info>
2016-05-02 23:04:55 -0400

    reverse user

--- scripts/ETHERPAD/10-create_user.sh
+++ scripts/ETHERPAD/10-create_user.sh
@@ -4,6 +4,6 @@ username=etherpad
 gecos="Etherpad User"
 
 $ROOTCMD getent passwd $username
-if [[ $? -eq 0 ]]; then
+if [[ ! $? -eq 0 ]]; then
     $ROOTCMD adduser --system --disabled-password --gecos "$gecos" $username
 fi
