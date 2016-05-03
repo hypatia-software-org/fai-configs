1d663febbdd229c1085078ba0b5b7c35f12c96f8
Tiffany <syn@synerfiles.info>
2016-05-02 23:25:40 -0400

    chown

--- scripts/ETHERPAD/10-create_user.sh
+++ scripts/ETHERPAD/10-create_user.sh
@@ -7,3 +7,5 @@ $ROOTCMD getent passwd $username
 if [[ ! $? -eq 0 ]]; then
     $ROOTCMD adduser --system --disabled-password --gecos "$gecos" $username
 fi
+
+$ROOTCMD chown -R $username:$username /usr/src/etherpad
