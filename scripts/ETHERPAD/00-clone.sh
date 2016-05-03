23afe207af29d867445ac547ad6c1ce35c74ca37
Tiffany <syn@synerfiles.info>
2016-05-03 00:38:12 -0400

    add etherpad settings

--- scripts/ETHERPAD/00-clone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-repolocation="https://github.com/ether/etherpad-lite.git"
-
-if [[ ! -d $target/usr/src/etherpad ]]; then
-    $ROOTCMD git clone $repolocation /usr/src/etherpad
-fi
--- scripts/ETHERPAD/30-setup_etherpad.sh
+++ scripts/ETHERPAD/30-setup_etherpad.sh
@@ -2,10 +2,17 @@
 
 username=etherpad
 gecos="Etherpad User"
+repolocation="https://github.com/ether/etherpad-lite.git"
 
 $ROOTCMD getent passwd $username
 if [[ ! $? -eq 0 ]]; then
     $ROOTCMD adduser --system --disabled-password --gecos "$gecos" $username
 fi
+
+if [[ ! -d $target/usr/src/etherpad ]]; then
+    $ROOTCMD git clone $repolocation /usr/src/etherpad
+fi
+
+fcopy /usr/src/etherpad/settings.json
 
 $ROOTCMD chown -R $username /usr/src/etherpad
