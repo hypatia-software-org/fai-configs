779a6ae214a43c4967bb0dfcbc5a0ce7ebed4af1
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2017-06-20 22:32:14 +0000

    moved proxy domains to var

--- scripts/PROXY/10-copy_scripts.sh
+++ scripts/PROXY/10-copy_scripts.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 fcopy /usr/local/bin/letsencrypt.sh
+
+$ROOTCMD sed "s/LETSENCRYPT_PATH/$LETSENCRYPT_PATH/g" -i /usr/local/bin/letsencrypt.sh
+$ROOTCMD sed "s/LETSENCRYPT_EMAIL/$LETSENCRYPT_EMAIL/g" -i /usr/local/bin/letsencrypt.sh
+
+#Copy domains
+dlist=''
+for domain in $LETSENCRYPT_DOMAINS; do
+    $dlist=$dlist"-e $domain "
+done
+
+$ROOTCMD sed "s/LETSENCRYPT_DOMAINS/$dlist/g" -i /usr/local/bin/letsencrypt.sh
