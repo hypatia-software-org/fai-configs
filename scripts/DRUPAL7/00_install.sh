d3b37db4ea6539075d4aaa23ecbc4acb29d0427e
ensra <ensra@users.noreply.github.com>
2018-03-22 19:01:20 +0000

    drupal: remove branch from script

--- scripts/DRUPAL7/00_install.sh
+++ scripts/DRUPAL7/00_install.sh
@@ -4,12 +4,10 @@ base=/var/www
 ver=7.57
 user=www-data
 
-if [[ ! -d $target$base ]]; then
-	$ROOTCMD rm -rf $base/*
-	curl -L https://ftp.drupal.org/files/projects/drupal-$ver.tar.gz | tar xzf - -C$target$base/
-	$ROOTCMD mv $base/drupal-$ver/* $base/drupal-$ver/.htaccess $base/drupal-$ver/.gitignore $base
-	$ROOTCMD cp $base/sites/default/default.settings.php $base/sites/default/settings.php 
-	$ROOTCMD chown $user $base/sites/default/settings.php 
-	$ROOTCMD mkdir $base/sites/default/files
-	$ROOTCMD chown $user $base/sites/default/files
-fi
+$ROOTCMD rm -rf $base/*
+curl -L https://ftp.drupal.org/files/projects/drupal-$ver.tar.gz | tar xzf - -C$target$base/
+$ROOTCMD mv $base/drupal-$ver/* $base/drupal-$ver/.htaccess $base/drupal-$ver/.gitignore $base
+$ROOTCMD cp $base/sites/default/default.settings.php $base/sites/default/settings.php
+$ROOTCMD chown $user $base/sites/default/settings.php
+$ROOTCMD mkdir $base/sites/default/files
+$ROOTCMD chown $user $base/sites/default/files
