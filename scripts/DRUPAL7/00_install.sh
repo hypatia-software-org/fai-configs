ea7e6329208beddd210c1a4fc596a31bf2d9b133
ensra <ensra@users.noreply.github.com>
2018-03-22 23:05:38 +0000

    drupal7: this doesn't need to be recursive

--- scripts/DRUPAL7/00_install.sh
+++ scripts/DRUPAL7/00_install.sh
@@ -42,6 +42,6 @@ $ROOTCMD cat <<EOM > /etc/apache2/sites-enabled/000-drupal.conf
 EOM
 $ROOTCMD a2dissite 000-default
 $ROOTCMD a2ensite 000-drupal
-$ROOTCMD rm -rf /etc/apache2/sites-enabled/000-default.conf
+$ROOTCMD rm -f /etc/apache2/sites-enabled/000-default.conf
 $ROOTCMD a2enmod rewrite
 $ROOTCMD service apache2 restart
