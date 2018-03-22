0d5b706014a92886b34e89189b5b924e3d916bee
ensra <ensra@users.noreply.github.com>
2018-03-22 21:39:47 +0000

    drupal7: mrow

--- scripts/DRUPAL7/00_install.sh
+++ scripts/DRUPAL7/00_install.sh
@@ -4,8 +4,10 @@ base=/var/www
 ver=7.57
 user=www-data
 
+$ROOTCMD curl -L https://ftp.drupal.org/files/projects/drupal-$ver.tar.gz -o /tmp/drupal-$ver.tar.gz
 $ROOTCMD mkdir -p $base
-$ROOTCMD curl -L https://ftp.drupal.org/files/projects/drupal-$ver.tar.gz | tar xzf - -C$base/
+$ROOTCMD tar xzf /tmp/drupal-$ver.tar.gz -C$base/
+$ROOTCMD rm -f /tmp/drupal-$ver.tar.gz
 $ROOTCMD mv $base/drupal-$ver/* $base/drupal-$ver/.htaccess $base/drupal-$ver/.gitignore $base
 $ROOTCMD cp $base/sites/default/default.settings.php $base/sites/default/settings.php
 $ROOTCMD chown $user $base/sites/default/settings.php
