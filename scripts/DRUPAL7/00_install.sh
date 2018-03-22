29e8fc620d6f732b0a37b1e278dbd96997c8be73
ensra <ensra@users.noreply.github.com>
2018-03-22 20:52:34 +0000

    drupal7: this seems unnecessary

--- scripts/DRUPAL7/00_install.sh
+++ scripts/DRUPAL7/00_install.sh
@@ -5,7 +5,6 @@ ver=7.57
 user=www-data
 
 $ROOTCMD mkdir -p $base
-$ROOTCMD rm -rf $base/*
 $ROOTCMD curl -L https://ftp.drupal.org/files/projects/drupal-$ver.tar.gz | tar xzf - -C$base/
 $ROOTCMD mv $base/drupal-$ver/* $base/drupal-$ver/.htaccess $base/drupal-$ver/.gitignore $base
 $ROOTCMD cp $base/sites/default/default.settings.php $base/sites/default/settings.php
