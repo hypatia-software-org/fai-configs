1cf2e27462d93e44beb83c113dcdd9d760ce8e5b
ensra <ensra@users.noreply.github.com>
2018-03-22 21:55:29 +0000

    drupal7: missing $target

--- scripts/DRUPAL7/00_install.sh
+++ scripts/DRUPAL7/00_install.sh
@@ -10,11 +10,11 @@ $ROOTCMD tar xzf /tmp/drupal-$ver.tar.gz -C$base/
 $ROOTCMD rm -f /tmp/drupal-$ver.tar.gz
 $ROOTCMD ls -l $base
 $ROOTCMD sh -c "mv $base/drupal-$ver/* $base/drupal-$ver/.htaccess $base/drupal-$ver/.gitignore $base"
-if [ ! -f $base/sites/default/settings.php ]; then
+if [ ! -f $target$base/sites/default/settings.php ]; then
 	$ROOTCMD cp $base/sites/default/default.settings.php $base/sites/default/settings.php
 	$ROOTCMD chown $user $base/sites/default/settings.php
 fi
-if [ ! -d $base/sites/default/files ]; then
+if [ ! -d $target$base/sites/default/files ]; then
 	$ROOTCMD mkdir $base/sites/default/files
 	$ROOTCMD chown $user $base/sites/default/files
 fi
