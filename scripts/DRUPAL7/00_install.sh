3981b65dde6ecb68f3618ad75e06d0831cd473bd
ensra <ensra@users.noreply.github.com>
2018-03-22 20:44:04 +0000

    drupal7: make and delete dirs

--- scripts/DRUPAL7/00_install.sh
+++ scripts/DRUPAL7/00_install.sh
@@ -4,6 +4,7 @@ base=/var/www
 ver=7.57
 user=www-data
 
+$ROOTCMD mkdir -p $base
 $ROOTCMD rm -rf $base/*
 $ROOTCMD curl -L https://ftp.drupal.org/files/projects/drupal-$ver.tar.gz | tar xzf - -C$base/
 $ROOTCMD mv $base/drupal-$ver/* $base/drupal-$ver/.htaccess $base/drupal-$ver/.gitignore $base
@@ -11,3 +12,5 @@ $ROOTCMD cp $base/sites/default/default.settings.php $base/sites/default/setting
 $ROOTCMD chown $user $base/sites/default/settings.php
 $ROOTCMD mkdir $base/sites/default/files
 $ROOTCMD chown $user $base/sites/default/files
+$ROOTCMD rm -rf $base/drupal-$ver
+$ROOTCMD rm -rf $base/html
