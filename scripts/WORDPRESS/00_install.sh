714d4787b44fde5fe8ea72e033845fa549b73835
ensra <ensra@users.noreply.github.com>
2018-03-23 13:12:19 +0000

    drupal -> wordpress

--- scripts/WORDPRESS/00_install.sh
+++ scripts/WORDPRESS/00_install.sh
@@ -10,7 +10,7 @@ curl -L https://wordpress.org/latest.tar.gz | zcat - | $ROOTCMD tar xf - -C$base
 $ROOTCMD ls -l $base
 $ROOTCMD sh -c "mv $base/wordpress/* $base"
 # Delete dirs that are no longer needed
-$ROOTCMD rm -rf $base/drupal-$ver
+$ROOTCMD rm -rf $base/wordpress
 $ROOTCMD rm -rf $base/html
 # Install the Apache configuration
 $ROOTCMD ls -l /etc/apache2
