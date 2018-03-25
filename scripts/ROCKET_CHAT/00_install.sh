8e97e2c410631f277d34dd53b9120d8119ae13e7
ensra <ensra@users.noreply.github.com>
2018-03-25 18:36:11 +0100

    different way of installing rocket chat

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-# I don't understand why this chmod stuff is needed yet
-uid=`ls -l $target | grep bin | cut -d' ' -f5 | head -1`
 
 curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
 $ROOTCMD apt-get install -y nodejs
+
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
-chown -R $uid:$uid $target/bundle
 $ROOTCMD mv bundle Rocket.Chat
-$ROOTCMD sh -c "cd /Rocket.Chat/programs/server && npm install"
-chown -R $uid:$uid $target/root/.npm
+
+fcopy -Bv /etc/init.d/rocket.chat
+$ROOTCMD update-rc.d rocket.chat defaults
