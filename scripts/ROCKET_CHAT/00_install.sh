f8b71bec9c29679d509abd4ee9fbde6abb301773
ensra <ensra@users.noreply.github.com>
2018-03-25 16:07:27 +0100

    rocket_chat: Use su?? Maybe the environment needs to be preserved?

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -3,4 +3,4 @@ curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
 $ROOTCMD apt-get install -y nodejs
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
 $ROOTCMD mv bundle Rocket.Chat
-cd $target/Rocket.Chat && $ROOTCMD npm install
+$ROOTCMD su -c "cd /Rocket.Chat && npm install"
