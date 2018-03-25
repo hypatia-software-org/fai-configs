2a75ee664570145d3060b50f342a9ee45cabc648
ensra <ensra@users.noreply.github.com>
2018-03-25 14:31:35 +0100

    rocket_chat: install nodejs the way it tells you on the nodejs site

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-$ROOTCMD npm install -g n
-$ROOTCMD n 8.9.3
+curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
+$ROOTCMD apt-get install -y nodejs
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
 $ROOTCMD "cd Rocket.Chat && npm install"
 $ROOTCMD rm -r Rocket.Chat
