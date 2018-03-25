d6764bafacf790a0108268c0718676cb80378489
ensra <ensra@users.noreply.github.com>
2018-03-25 16:31:54 +0100

    rocket_chat: use head

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -5,5 +5,5 @@ curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf 
 $ROOTCMD mv bundle Rocket.Chat
 $ROOTCMD sh -c "cd /Rocket.Chat && npm install"
 # I don't understand why this is needed yet
-uid=`ls -l $target | grep bin | cut -d' ' -f5`
+uid=`ls -l $target | grep bin | cut -d' ' -f5 | head -1`
 chown -R $uid:$uid $target/root/.npm
