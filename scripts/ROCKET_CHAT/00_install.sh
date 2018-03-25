76cba84c64ff10429bd2263f7df187823e1270ef
ensra <ensra@users.noreply.github.com>
2018-03-25 16:26:05 +0100

    rocket_chat: Use chown

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -3,4 +3,7 @@ curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
 $ROOTCMD apt-get install -y nodejs
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
 $ROOTCMD mv bundle Rocket.Chat
-$ROOTCMD su -c "cd /Rocket.Chat && npm install"
+$ROOTCMD sh -c "cd /Rocket.Chat && npm install"
+# I don't understand why this is needed yet
+uid=`ls -l $target | grep bin | cut -d' ' -f5`
+chown -R $uid:$uid $target/root/.npm
