fbfab94c02395d628b947e52aac8048a9df40ad9
ensra <ensra@users.noreply.github.com>
2018-03-25 20:22:06 +0100

    more moving stuff to init.d

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
-$ROOTCMD apt-get install -y nodejs
-
-curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf --no-same-owner -
-$ROOTCMD mv bundle Rocket.Chat
-
 fcopy -Bv /etc/init.d/rocket.chat
 $ROOTCMD update-rc.d rocket.chat defaults
