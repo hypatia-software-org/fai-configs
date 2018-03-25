32686e8a11f0ca604fd64677f495a1912417ef64
ensra <ensra@users.noreply.github.com>
2018-03-25 14:39:43 +0100

    rocket_chat: add ls

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -2,5 +2,6 @@
 curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
 $ROOTCMD apt-get install -y nodejs
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
+$ROOTCMD ls -l
 $ROOTCMD "cd Rocket.Chat && npm install"
 $ROOTCMD rm -r Rocket.Chat
