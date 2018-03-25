5128b0ca98398bc26a324636901c8fffe1e0330b
ensra <ensra@users.noreply.github.com>
2018-03-25 13:37:04 +0100

    rocket_chat: ls different directory

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -4,6 +4,6 @@ $ROOTCMD n 8.9.3
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
 $ROOTCMD mv bundle Rocket.Chat
 $ROOTCMD ls -l
-$ROOTCMD ls -l bundle
+$ROOTCMD ls -l Rocket.Chat
 $ROOTCMD sh -c "cd Rocket.Chat/Programs/server && npm install"
 
