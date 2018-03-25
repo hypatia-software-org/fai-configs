9240769aa22ce8cfe35a4aa3316068d77b723e0d
ensra <ensra@users.noreply.github.com>
2018-03-25 14:50:47 +0100

    rocket_chat: keep this dir around

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -2,5 +2,5 @@
 curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
 $ROOTCMD apt-get install -y nodejs
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
-$ROOTCMD sh -c "cd bundle && npm install"
-$ROOTCMD rm -r bundle
+$ROOTCMD mv bundle Rocket.Chat
+$ROOTCMD sh -c "cd Rocket.Chat && npm install"
