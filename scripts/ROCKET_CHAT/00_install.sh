ee9d755c9fedc00114232463888faa88ce72a917
ensra <ensra@users.noreply.github.com>
2018-03-25 13:31:11 +0100

    rocket_chat: add debug statements

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -3,5 +3,7 @@ $ROOTCMD npm install -g n
 $ROOTCMD n 8.9.3
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
 $ROOTCMD mv bundle Rocket.Chat
+$ROOTCMD ls -l
+$ROOTCMD ls -l bundle
 $ROOTCMD sh -c "cd Rocket.Chat/Programs/server && npm install"
 
