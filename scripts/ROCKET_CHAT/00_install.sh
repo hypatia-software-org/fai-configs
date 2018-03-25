630f068142831d94fc271fe86bb23c4218fa40e6
ensra <ensra@users.noreply.github.com>
2018-03-25 13:57:05 +0100

    rocket_chat: fix dir name

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -2,5 +2,5 @@
 $ROOTCMD npm install -g n
 $ROOTCMD n 8.9.3
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
-$ROOTCMD "cd bundle && npm install"
-$ROOTCMD rm -r bundle
+$ROOTCMD "cd Rocket.Chat && npm install"
+$ROOTCMD rm -r Rocket.Chat
