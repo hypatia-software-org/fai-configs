b3922bd27ccd2317a85aa7af49d21a3d55d684c9
ensra <ensra@users.noreply.github.com>
2018-03-25 13:27:18 +0100

    add rocketjail0

---
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+$ROOTCMD npm install -g n
+$ROOTCMD n 8.9.3
+curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
+$ROOTCMD mv bundle Rocket.Chat
+$ROOTCMD sh -c "cd Rocket.Chat/Programs/server && npm install"
+
