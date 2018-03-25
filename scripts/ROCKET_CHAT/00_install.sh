24f549ce8747a76cf538668fd5a7eb72d465dee1
ensra <ensra@users.noreply.github.com>
2018-03-25 14:58:12 +0100

    rocket_chat: change how npm invokation works

--- scripts/ROCKET_CHAT/00_install.sh
+++ scripts/ROCKET_CHAT/00_install.sh
@@ -3,4 +3,4 @@ curl -L https://deb.nodesource.com/setup_8.x | $ROOTCMD bash -
 $ROOTCMD apt-get install -y nodejs
 curl -L https://releases.rocket.chat/latest/download | zcat - | $ROOTCMD tar xf -
 $ROOTCMD mv bundle Rocket.Chat
-$ROOTCMD sh -c "cd Rocket.Chat && npm install"
+cd Rocket.Chat && $ROOTCMD npm install
