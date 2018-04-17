3fbd3f3794dda5ec9c6171e1d8e01ffd0395aeeb
ensra <ensra@users.noreply.github.com>
2018-04-17 22:34:05 +0100

    nginx_proxy: always set host header

--- scripts/NGINX_PROXY/20-copy_configs.sh
+++ scripts/NGINX_PROXY/20-copy_configs.sh
@@ -26,11 +26,11 @@ EOF
 		proxy_redirect off;
 		proxy_set_header Upgrade \$http_upgrade;
 		proxy_set_header Connection "upgrade";
-		proxy_set_header Host \$http_host;
 EOF
 	fi
 		cat >> /etc/nginx/sites-available/$public.conf <<EOF
 
+		proxy_set_header Host \$http_host;
 		proxy_set_header X-Real-IP \$remote_addr;
 		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 		proxy_set_header X-Forwarded-Proto http;
