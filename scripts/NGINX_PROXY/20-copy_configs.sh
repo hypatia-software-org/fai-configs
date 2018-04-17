5c196558847ba74f0add9eb14cd756e07adf92f4
ensra <ensra@users.noreply.github.com>
2018-04-17 21:45:59 +0100

    nginx_proxy: handle rocket specially, tabs to spaces, variable changes

--- scripts/NGINX_PROXY/20-copy_configs.sh
+++ scripts/NGINX_PROXY/20-copy_configs.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 echo $PROXY_DOMAINS
 for domainpair in $PROXY_DOMAINS; do
-    echo o
-    public=`echo $domainpair | cut -d: -f1`
-    echo $public
-    private=`echo $domainpair | cut -d: -f2`
-    echo $private
-    port=`echo $domainpair | cut -d: -f3`
-    echo $port
-    if [ "" = "$port" ]; then
-       port=80
-    fi
-    cat > /etc/nginx/sites-available/$public.conf <<EOF
+	public=`echo $domainpair | cut -d: -f1`
+	private=`echo $domainpair | cut -d: -f2`
+	port=`echo $domainpair | cut -d: -f3`
+	if [ "" = "$port" ]; then
+	   port=80
+	fi
+	cat > /etc/nginx/sites-available/$public.conf <<EOF
 server {
-    listen 443;
-    server_name $public;
+	listen 443;
+	server_name $public;
 
-    error_log /var/log/nginx/$public.error.log;
+	error_log /var/log/nginx/$public.error.log;
 
-    include /etc/nginx/ssl.conf;
+	include /etc/nginx/ssl.conf;
 
-    location / {
-	    proxy_pass http://$private:$port/;
-	    proxy_http_version 1.1;
-	    proxy_set_header Upgrade \$http_upgrade;
-	    proxy_set_header Connection "upgrade";
-	    proxy_set_header Host \$http_host;
+EOF
+	if [ $public = "chat.hypatiasoftware.org" ]; then
+		cat >> /etc/nginx/sites-available/$public.conf <<EOF
+	location / {
+		proxy_pass http://$private:$port/;
+		proxy_http_version 1.1;
+		proxy_set_header Upgrade \$http_upgrade;
+		proxy_set_header Connection "upgrade";
+		proxy_set_header Host \$http_host;
 
-	    proxy_set_header X-Real-IP \$remote_addr;
-	    proxy_set_header X-Forward-For \$proxy_add_x_forwarded_for;
-	    proxy_set_header X-Forward-Proto http;
-	    proxy_set_header X-Nginx-Proxy true;
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		proxy_set_header X-Forwarded-Proto http;
 
-	    proxy_redirect off;
-    }
+		proxy_redirect off;
+	}
 }
 EOF
-    ln -s /etc/nginx/sites-available/$public.conf /etc/nginx/sites-enabled/
+	else
+		cat >> /etc/nginx/sites-available/$public.conf <<EOF
+	location / {
+		proxy_pass http://$private:$port/;
+		proxy_http_version 1.1;
+
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		proxy_set_header X-Forwarded-Proto http;
+
+		proxy_redirect off;
+	}
+}
+EOF
+	fi
+	ln -s /etc/nginx/sites-available/$public.conf /etc/nginx/sites-enabled/
 done
 service nginx restart
