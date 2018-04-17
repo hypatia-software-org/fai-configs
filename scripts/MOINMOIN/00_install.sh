6df62489e05b1770f4e59a92f514b5f2faf32de8
ensra <ensra@users.noreply.github.com>
2018-04-18 00:45:50 +0100

    moinmoin: add missing stuff to install script

--- scripts/MOINMOIN/00_install.sh
+++ scripts/MOINMOIN/00_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 fcopy -Bv /etc/nginx/sites-available/moin.conf
 ln -s /etc/nginx/sites-enabled/moin.conf /etc/nginx/sites-available/moin.conf
-service nginx restart
-
-
+fcopy -Bv /etc/init.d/moinmoin
+$ROOTCMD update-rc.d moinmoin defaults
+$ROOTCMD service nginx restart
