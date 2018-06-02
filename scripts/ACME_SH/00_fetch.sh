e43d9b89678031aee8a5a1da33e209ffdde0677f
ensra <ensra@users.noreply.github.com>
2018-06-02 16:06:56 +0100

    fcopy acme.sh files

--- scripts/ACME_SH/00_fetch.sh
+++ scripts/ACME_SH/00_fetch.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 $ROOTCMD curl -L https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh -o /usr/local/bin/acme.sh
 $ROOTCMD chmod +x /usr/local/bin/acme.sh
+fcopy -Bv /usr/local/bin/acme-update.sh
+fcopy -Bv /etc/cron.d/acme
