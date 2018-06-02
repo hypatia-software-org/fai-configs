6e015d92df66baa5cd86c4e7b3862b1648021f18
ensra <ensra@users.noreply.github.com>
2018-06-02 16:04:07 +0100

    acme updates

--- scripts/ACME_SH/00_fetch.sh
+++ scripts/ACME_SH/00_fetch.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -L https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh -o /usr/local/bin/acme.sh
+$ROOTCMD curl -L https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh -o /usr/local/bin/acme.sh
+$ROOTCMD chmod +x /usr/local/bin/acme.sh
