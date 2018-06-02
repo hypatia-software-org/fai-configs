f29d5317d8e21d26ea51e2de95c789e94c18b0f4
ensra <ensra@users.noreply.github.com>
2018-06-02 16:10:16 +0100

    acme.sh: sed replace variable in acme-update.sh

--- scripts/ACME_SH/00_fetch.sh
+++ scripts/ACME_SH/00_fetch.sh
@@ -3,3 +3,4 @@ $ROOTCMD curl -L https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.
 $ROOTCMD chmod +x /usr/local/bin/acme.sh
 fcopy -Bv /usr/local/bin/acme-update.sh
 fcopy -Bv /etc/cron.d/acme
+$ROOTCMD sed "s/LETSENCRYPT_DOMAINS/$LETSENCRYPT_DOMAINS/g" -i /usr/local/bin/acme-update.sh
