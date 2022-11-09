090189713cc7612612048090b5d69964c00b4852
Lisa Marie <lisa@westvirginia.express>
2022-11-09 20:31:41 +0000

    hack attempt 2

--- scripts/NODEJS/00-install-nodejs.sh
+++ scripts/NODEJS/00-install-nodejs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-$ROOTCMD curl -sL https://deb.nodesource.com/setup_16.x | $ROOTCMD bash - 
+curl -sL https://deb.nodesource.com/setup_16.x | $ROOTCMD bash - 
 $ROOTCMD apt-get -y install nodejs
