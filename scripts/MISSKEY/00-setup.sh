bf31b51f724e348135f234d3227ed66068e30116
Lisa Marie <lisa@westvirginia.express>
2022-11-09 19:48:20 +0000

    cd

--- scripts/MISSKEY/00-setup.sh
+++ scripts/MISSKEY/00-setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 $ROOTCMD npm install -g yarn
 $ROOTCMD adduser --disabled-password --disabled-login misskey --gecos "misskey user"
-$ROOTCMD sudo -u misskey -D /home/misskey git clone --recursive https://github.com/misskey-dev/misskey.git
-$ROOTCMD sudo -u misskey -D /home/misskey/misskey git checkout master
-$ROOTCMD sudo -u misskey -D /home/misskey/misskey yarn install
-$ROOTCMD sudo -u misskey -D /home/misskey/misskey cp .config/example.yml .config/default.yml
-$ROOTCMD sudo -u misskey -D /home/misskey/misskey NODE_ENV=production yarn build
-$ROOTCMD sudo -u misskey -D /home/misskey/misskey yarn run init
-$ROOTCMD sudo -u misskey -D /home/misskey/misskey /etc/systemd/system/misskey.service
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey; git clone --recursive https://github.com/misskey-dev/misskey.git"
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey/misskey; git checkout master"
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey/misskey; yarn install"
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey/misskey; cp .config/example.yml .config/default.yml"
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey/misskey; NODE_ENV=production yarn build"
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey/misskey; yarn run init"
+$ROOTCMD sudo -u misskey sh -c "cd /home/misskey/misskey; /etc/systemd/system/misskey.service"
 fcopy /etc/systemd/system/misskey.service
 $ROOTCMD sudo -u misskey systemctl daemon-reload
 $ROOTCMD sudo -u misskey systemctl enable misskey
