8544d100dd4aeb8efb099a156c81e01e360d5ae3
Lisa Marie <lisa@westvirginia.express>
2022-11-09 19:59:43 +0000

    scope issue

--- scripts/NODEJS/00-install-nodejs.sh
+++ scripts/NODEJS/00-install-nodejs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$ROOTCMD curl -sL https://deb.nodesource.com/setup_16.x | bash -
+$ROOTCMD curl -sL https://deb.nodesource.com/setup_16.x | $ROOTCMD bash -
 $ROOTCMD apt update
 $ROOTCMD apt-get -y install nodejs
